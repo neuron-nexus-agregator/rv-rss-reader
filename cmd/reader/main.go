ad346a70d9b4676e17c2f8c2b9a3b369f986e959
neuron-nexus-agregator <gafarov.neuron.nexus@gmail.com>
2025-12-11 12:31:38 +0300

    Zap logger added

--- cmd/reader/main.go
+++ cmd/reader/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func init_system() error {
+func initSystem() error {
 	err := godotenv.Load()
 	if err != nil {
 		return fmt.Errorf("error loading .env file")
@@ -14,7 +18,76 @@ func init_system() error {
 	return nil
 }
 
+func initLogger() *zap.Logger {
+	// Создаем папку logs, если ее нет
+	_ = os.MkdirAll("logs", 0755)
+
+	// Основной файл: ротация каждые 3 часа
+	mainFile := &lumberjack.Logger{
+		Filename:   "logs/app.log",
+		MaxAge:     7,  // хранить 7 дней
+		MaxBackups: 56, // 24/3 * 7 = 56 ротаций
+		Compress:   false,
+	}
+
+	// Файл ошибок
+	errorFile := &lumberjack.Logger{
+		Filename:   "logs/errors.log",
+		MaxAge:     7,
+		MaxBackups: 56,
+		Compress:   false,
+	}
+
+	// Настройка формата JSON
+	encoderCfg := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		MessageKey:     "msg",
+		CallerKey:      "caller",
+		StacktraceKey:  "stack",
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+	}
+
+	encoder := zapcore.NewJSONEncoder(encoderCfg)
+
+	// Потоки
+	consoleWS := zapcore.AddSync(os.Stdout)
+	mainWS := zapcore.AddSync(mainFile)
+	errorWS := zapcore.AddSync(errorFile)
+
+	// Уровни
+	infoLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
+		return l >= zapcore.InfoLevel
+	})
+
+	errorLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
+		return l >= zapcore.ErrorLevel
+	})
+
+	// Основной поток: INFO+ в файл и в консоль
+	mainCore := zapcore.NewTee(
+		zapcore.NewCore(encoder, consoleWS, infoLevel),
+		zapcore.NewCore(encoder, mainWS, infoLevel),
+	)
+
+	// Дополнительный поток ошибок: ERROR+ в отдельный файл
+	errorCore := zapcore.NewCore(encoder, errorWS, errorLevel)
+
+	// Объединяем
+	core := zapcore.NewTee(mainCore, errorCore)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+
+	return logger
+}
+
 func main() {
+	logger := initLogger()
+	defer logger.Sync()
+
 	for i := range 3 {
 		fmt.Println(i)
 	}
--- go.mod
+++ go.mod
@@ -18,5 +18,8 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/segmentio/kafka-go v0.4.49 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
